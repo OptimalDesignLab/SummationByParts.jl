95165b37a57efc13e49abb5aa443db4a7a10240c
Jason Hicken <hickej2@rpi.edu>
2015-12-08 22:08:42 -0500

    replaced dependency on PDESolverCommon with ODLCommonTools

--- deps/build.jl
+++ deps/build.jl
@@ -1,8 +1,8 @@
 # SummationByParts.jl uses the following (very small) Package, which is not
 # listed in the METADATA; therefore, it must be added explicitly
-pdesolvercommon_git = "https://github.com/OptimalDesignLab/PDESolverCommon.jl.git"
+odlcommontools_git = "https://github.com/OptimalDesignLab/ODLCommonTools.jl.git"
 pkg_dict = Pkg.installed()
-if !haskey(pkg_dict, "PDESolverCommon")
-  Pkg.clone(pdesolvercommon_git)
-  Pkg.build("PDESolverCommon")
+if !haskey(pkg_dict, "ODLCommonTools")
+  Pkg.clone(odlcommontools_git)
+  Pkg.build("ODLCommonTools")
 end
