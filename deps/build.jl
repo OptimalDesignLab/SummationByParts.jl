1e26a7c7cc10ce27b84060166fe8dbff0bffaecc
Jared Crean <jcrean01@gmail.com>
2016-04-21 13:43:29 -0400

    another attempt at fixing build system

--- deps/build.jl
+++ deps/build.jl
@@ -1,7 +1,10 @@
 # SummationByParts.jl uses the following (very small) Package, which is not
 # listed in the METADATA; therefore, it must be added explicitly
 pkg_dict = Pkg.installed()
+start_dir = pwd()
+deps_path = joinpath(Pkg.dir("SummationByParts"), "deps")
+cd(deps_path)
 if !haskey(pkg_dict, "ODLCommonTools")
-  run(`download.sh`)
+  run(`./download.sh`)
   Pkg.build("ODLCommonTools")
 end
