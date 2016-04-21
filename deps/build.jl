063d6c793b1719c907f069ba679b3b3e2a7f7a33
Jared Crean <jcrean01@gmail.com>
2016-04-21 13:27:19 -0400

    create download.sh for offline installation

--- deps/build.jl
+++ deps/build.jl
@@ -1,8 +1,7 @@
 # SummationByParts.jl uses the following (very small) Package, which is not
 # listed in the METADATA; therefore, it must be added explicitly
-odlcommontools_git = "https://github.com/OptimalDesignLab/ODLCommonTools.jl.git"
 pkg_dict = Pkg.installed()
 if !haskey(pkg_dict, "ODLCommonTools")
-  Pkg.clone(odlcommontools_git)
+  run(`download.sh`)
   Pkg.build("ODLCommonTools")
 end
---
+++ deps/download.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+odlcommontools_git="https://github.com/OptimalDesignLab/ODLCommonTools.jl.git"
+wget $odlcommontools_git
+mv -v ./ODLCommonTools.jl ./ODLCommonTools
+
