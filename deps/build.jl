a95fcdd64d69bbc53df37ebd91b54efc0d2c9acb
jared <jcrean01@gmail.com>
2018-02-27 19:43:55 -0500

    change build system to use PkgFix

--- deps/build.jl
+++ deps/build.jl
@@ -1,18 +1,34 @@
 # SummationByParts.jl uses the following (very small) Package, which is not
 # listed in the METADATA; therefore, it must be added explicitly
-pkg_dict = Pkg.installed()
-start_dir = pwd()
-deps_path = joinpath(Pkg.dir("SummationByParts"), "deps")
-cd(deps_path)
+
+# install PkgFix if not present
+if !isdir(joinpath(Pkg.dir(), "PkgFix"))
+  Pkg.clone("PkgFix")
+end
+
+using PkgFix  # from now on, use PkgFix instead of Pkg for everything
+
+ODL_URL = "https://github.com/OptimalDesignLab/ODLCommonTools.jl.git"
+ODL_VER = "new_parallel"
+
+
+
+pkg_dict = PkgFix.installed()
+#start_dir = pwd()
+#deps_path = joinpath(Pkg.dir("SummationByParts"), "deps")
+#cd(deps_path)
 if !haskey(pkg_dict, "ODLCommonTools")
+  PkgFix.add(ODL_URL, branch_ish=OLD_VER)
+  #=
   run(`./download.sh`)
   start_dir2 = pwd()
   cd(Pkg.dir("ODLCommonTools"))
   run(`git checkout new_parallel`)
   cd(start_dir2)
   Pkg.build("ODLCommonTools")
+  =#
 end
-cd(start_dir)
+#cd(start_dir)
 
 # get the right commit of ArrayViews (before the aview change)
 dir = Pkg.dir("ArrayViews")
