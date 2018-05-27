cd6ff2087a8c2950c188f4801899b67bb6606823
Jared Crean <jcrean01@gmail.com>
2018-05-27 19:36:02 -0400

    start upgrading to 0.6

--- deps/build.jl
+++ deps/build.jl
@@ -5,17 +5,18 @@
 if !isdir(joinpath(Pkg.dir(), "PkgFix"))
   Pkg.clone("https://github.com/OptimalDesignLab/PkgFix.jl.git")
 end
+Pkg.checkout("PkgFix", "upgrade_0.6")
 
 using PkgFix  # from now on, use PkgFix instead of Pkg for everything
 
 ODL_URL = "https://github.com/OptimalDesignLab/ODLCommonTools.jl.git"
 ODL_VER = "update_0.5"
-ARRAYVIEWS_VER = "93e80390aeedb1dbcd90281b6dff7f760f430bc8"
+ARRAYVIEWS_VER = "master"
 
 
 pkg_dict = PkgFix.installed()
 if !haskey(pkg_dict, "ODLCommonTools")
   PkgFix.add(ODL_URL, branch_ish=ODL_VER)
 end
-# get the right commit of ArrayViews (before the aview change)
+# get the right commit of ArrayViews (after the aview change)
 PkgFix.checkout("ArrayViews", ARRAYVIEWS_VER)
