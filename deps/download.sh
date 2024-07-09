21cb3ee629dbd80ecfa9f1eab9f6e79ae9761239
Zelalem <zolargwrk@gmail.com>
2024-07-08 23:39:09 -0400

    documenter kine of working...but not yet clean.

--- deps/download.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-odlcommontools_git="https://github.com/OptimalDesignLab/ODLCommonTools.jl.git"
-if [ ! -d "ODLCommonTools" ]; then
-  git clone $odlcommontools_git ../../ODLCommonTools
-fi
-
-
